59dcff2635beef3bcb582d5d7c648116dcac796d
vamshi-1216 <bvamshi1817@gmail.com>
2026-02-23 15:48:51 +0530

    update website changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,18 @@ export default function Navbar() {
     <nav className="w-full fixed top-0 left-0 z-50">
       <div className="bg-gradient-to-r from-blue-900/95 to-blue-600/95 backdrop-blur-md shadow-md">
 
-        {/* Increased navbar height */}
         <div className="mx-auto max-w-7xl px-7 h-20 flex items-center justify-between">
 
-          {/* Logo */}
-          <NavLink to="/" className="flex items-center">
+          {/* Logo + Brand Name (using favicon) */}
+          <NavLink to="/" className="flex flex-col items-center leading-none">
             <img
-              src="/icons/SkyBridgeLogo.png"
+              src="/favicon.png"
               alt="SkyBridge ITC"
-              className="h-12 w-auto object-contain"
+              className="h-11 w-11 object-contain"
             />
+            <h1 className="text-white text-lg font-semibold tracking-wide -mt-1">
+              SkyBridge <span className="font-light">ITC</span>
+            </h1>
           </NavLink>
 
           {/* Navigation Links */}
